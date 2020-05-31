5cc3ddd9eac17391b5c7aa7e0c2669401a257c03
friedroyce <friedroyze@gmail.com>
2020-05-31 23:09:36 +0800

    added comments

--- app/src/main/java/com/example/reservationapp/AccountLoginActivity.java
+++ app/src/main/java/com/example/reservationapp/AccountLoginActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class AccountLoginActivity extends AppCompatActivity {
 
+    //variables for ui elements
     Button btnLogin, btnRegister;
     EditText txtStudentId, txtPassword;
 
@@ -25,6 +26,7 @@ public class AccountLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_login);
 
+        //linking ui elements to variables
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
         txtStudentId = findViewById(R.id.txtStudentId);
@@ -39,24 +41,30 @@ public class AccountLoginActivity extends AppCompatActivity {
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //get current values in text field
                 final String studentId = txtStudentId.getText().toString();
                 final String pass = txtPassword.getText().toString();
+
                 if (studentId.isEmpty() || pass.isEmpty()){
                     ShowToast("Please enter your ID and password");
                 }
                 else{
-                    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("Students");
-                    rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    //gets database reference of students
+                    DatabaseReference refStudents = FirebaseDatabase.getInstance().getReference("Students");
+                    refStudents.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
+                            //check if students has someone with that stud id
                             if (!dataSnapshot.hasChild(studentId))
                                 ShowToast("The account does not exist");
+                            //check if passwords match
                             else if (!dataSnapshot.child(studentId).child("password").getValue().toString().equals(pass))
                                 ShowToast("Incorrect credentials");
                             else {
                                 ShowToast("Login successful");
 
                                 Intent intent = new Intent(AccountLoginActivity.this, MainActivity.class);
+                                //pass student id in next activity
                                 intent.putExtra("studentId", studentId);
                                 startActivityForResult(intent,1);
                             }
