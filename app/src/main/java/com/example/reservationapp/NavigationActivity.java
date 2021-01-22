9ebcbc5c1f291f91566551b511d164f26ea2e557
roy <friedroyze@gmail.com>
2021-01-23 02:22:00 +0800

    deleted comments

--- app/src/main/java/com/example/reservationapp/NavigationActivity.java
+++ app/src/main/java/com/example/reservationapp/NavigationActivity.java
@@ -3,6 +3,7 @@ package com.example.reservationapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -20,6 +21,11 @@ public class NavigationActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragment);
+
+//        AppBarConfiguration configuration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, configuration);
+
+
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
     }
