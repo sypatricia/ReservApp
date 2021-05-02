aadfb7193c5ef77f6efb0fda451cf1f19a9ca8fa
sypatricia <sypatriciaaa@gmail.com>
2021-05-03 03:28:38 +0800

    added numeric keypad to student number prompt

--- app/src/main/java/com/example/reservationapp/AccountLoginActivity.java
+++ app/src/main/java/com/example/reservationapp/AccountLoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -43,6 +44,8 @@ public class AccountLoginActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        txtStudentId.setTransformationMethod(new NumericKeyBoardTransformationMethod());
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -87,4 +90,11 @@ public class AccountLoginActivity extends AppCompatActivity {
     }
 
     void ShowToast(String message){ Toast.makeText(this, message, Toast.LENGTH_SHORT).show(); }
+
+    private class NumericKeyBoardTransformationMethod extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return source;
+        }
+    }
 }
