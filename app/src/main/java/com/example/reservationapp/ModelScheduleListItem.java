e94f70ba1aea584110c92d7cf35d8cdf80ce4479
royce <friedroyze@gmail.com>
2021-05-08 02:54:22 +0800

    added schedule list item model

---
+++ app/src/main/java/com/example/reservationapp/ModelScheduleListItem.java
@@ -0,0 +1,17 @@
+package com.example.reservationapp;
+
+public class ModelScheduleListItem {
+    String time;
+
+    public ModelScheduleListItem(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
